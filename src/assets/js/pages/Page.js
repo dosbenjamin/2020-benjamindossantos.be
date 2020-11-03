0b9e45fc19c2c2a9b5da66039ccf26b6687937fc
dosbenjamin <dosbenjamin@gmail.com>
2020-11-03 20:59:41 +0100

    Add fade in and hover animations

--- src/assets/js/pages/Page.js
+++ src/assets/js/pages/Page.js
@@ -1,19 +1,30 @@
-import { $ } from '../utilities/domSelectors'
+import { $, $$ } from '../utilities/domSelectors'
 import Scrollbar from 'smooth-scrollbar'
+import anime from 'animejs/lib/anime.es.js'
+// import perspective from '../components/perspectiveAnimation'
 
 export default class Page {
   constructor (name) {
     this.name = name
-    this.width = window.innerWidth
-    this.height = window.innerHeight
-    this.$wrapper = $('.l-wrapper')
-    this.$wrapper.style.height = `${this.height}px`
+    this.sizes = { width: window.innerWidth, height: window.innerHeight }
+    this.$wrapper = $('.js-wrapper')
+    this.$wrapper.style.height = `${this.sizes.height}px`
     this.Scroll = Scrollbar.init(this.$wrapper)
+    this.$toFadeIn = $$('.js-fade-in')
+    anime({
+      targets: this.$toFadeIn,
+      translateY: [50, 0],
+      opacity: [0, 1],
+      duration: 1000,
+      delay: anime.stagger(100),
+      easing: 'cubicBezier(0, 0.55, 0.45, 1)'
+    })
+    // window.addEventListener('mousemove', event => perspective(event, this.sizes, this.$wrapper))
   }
 
   updateSizes () {
-    this.width = window.innerWidth
-    this.height = window.innerHeight
-    this.$wrapper.style.height = `${this.height}px`
+    this.sizes = { width: window.innerWidth, height: window.innerHeight }
+    this.$wrapper.style.height = `${this.sizes.height}px`
+    // perspective(event, this.sizes, this.$wrapper)
   }
 }
