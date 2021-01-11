b18fef4f52e46154e182c4c5fb979fe5a4dda882
dosbenjamin <dosbenjamin@icloud.com>
2021-01-11 20:30:25 +0100

    Fix animations delay

--- src/assets/js/components/animations/revealOnScroll.js
+++ src/assets/js/components/animations/revealOnScroll.js
@@ -12,38 +12,41 @@ export default ({
   })
   anime.timeline({
     easing: 'cubicBezier(0, 0.55, 0.45, 1)',
-    delay: anime.stagger(100, { start: 300 }),
     duration: 500
   }).add({
     targets: $fadeIn,
     opacity: [0, 1],
-    translateY: ['1em', 0]
+    translateY: ['1em', 0],
+    delay: anime.stagger(100, { start: 300 })
   }).add({
-    targets: $bigTranslate,
-    delay: anime.stagger(150),
-    duration: 1000,
-    opacity: [0, 1],
-    translateY: ['75%', 0],
-    rotateZ: [1, 0],
-    changeBegin: ({ animatables }) => {
-      animatables.forEach(({ target: $element }) => {
-        const $parentLink = $element.closest('.js-project-link')
-        $parentLink.style.pointerEvents = 'auto'
-      })
-    }
-  }, '-=300').add({
-    targets: $smallTranslate,
-    duration: 500,
-    opacity: [0, 1],
-    translateY: [20, 0]
-  }, '-=650').add({
-    targets: $title,
-    opacity: [0, 1],
-    translateY: ['-0.25em', 0],
-    rotateZ: ['0.75deg', 0]
-  }, '-=1000').add({
     targets: $fadeInReverse,
     opacity: [0, 1],
-    translateY: ['-0.25em', 0]
-  }, '-=1000')
+    translateY: ['-0.25em', 0],
+    duration: 750,
+    delay: anime.stagger(200)
+  })
+  // .add({
+  //   targets: $bigTranslate,
+  //   delay: anime.stagger(150),
+  //   duration: 1000,
+  //   opacity: [0, 1],
+  //   translateY: ['75%', 0],
+  //   rotateZ: [1, 0],
+  //   changeBegin: ({ animatables }) => {
+  //     animatables.forEach(({ target: $element }) => {
+  //       const $parentLink = $element.closest('.js-project-link')
+  //       $parentLink.style.pointerEvents = 'auto'
+  //     })
+  //   }
+  // }).add({
+  //   targets: $smallTranslate,
+  //   duration: 500,
+  //   opacity: [0, 1],
+  //   translateY: [20, 0]
+  // }).add({
+  //   targets: $title,
+  //   opacity: [0, 1],
+  //   translateY: ['-0.25em', 0],
+  //   rotateZ: ['0.75deg', 0]
+  // })
 }
